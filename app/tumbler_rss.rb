dfbddab0851b8f73de226c5a24b4f08448e7108a
Timur Vafin <timur.vafin@flatsoft.com>
2010-10-06 04:23:37 +0400

    Added index page

--- app/tumbler_rss.rb
+++ app/tumbler_rss.rb
@@ -3,6 +3,10 @@ require 'bundler/setup'
 require 'sinatra'
 require 'tumblr_dashboard_rss'
 
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
 get '/:email/:password' do
   Tumblr::DashboardRSS.new(params[:email], params[:password], params).to_rss
 end
